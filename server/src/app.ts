be4eba38cbfc1efb5335beff60f00c978a908f24
Lex Yehia <lex.yehia@icloud.com>
2017-11-02 13:42:44 -0400

    Removed morgan from production

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,4 @@
 import * as express from 'express'
-import * as morgan from 'morgan'
 import * as http from 'http'
 import * as path from 'path'
 import ChatSocketServer from './sockets/chat'
@@ -12,7 +11,6 @@ const PORT: number = Number(process.env.PORT) || 3001
  */
 const app: any = express()
 
-app.use(morgan('dev'))
 app.use('/public', express.static('./client/dist'))
 
 app.get('/', (req: any, res: any) => {
